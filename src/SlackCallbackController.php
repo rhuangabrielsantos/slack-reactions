7f63435714549ef2c0919379b4806e98b7677c99
rhuangabrielsantos <rhuan.santos@db1.com.br>
2021-05-26 15:38:05 -0300

    :rocket: Test integration

--- src/SlackCallbackController.php
+++ src/SlackCallbackController.php
@@ -9,6 +9,10 @@ class SlackCallbackController
 {
     public function handler(?array $data): array
     {
+        if (!is_file(__DIR__.'/my_app.log')) {
+            file_put_contents(__DIR__ . '/my_app.log', '');
+        }
+
         $logger = new Logger('teste');
         $logger->pushHandler(new StreamHandler(__DIR__.'/my_app.log', Logger::DEBUG));
 
