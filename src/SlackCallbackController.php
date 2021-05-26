a2487e194d73a970b1ede31b75f60f1827c29957
rhuangabrielsantos <rhuan.santos@db1.com.br>
2021-05-26 15:08:42 -0300

    :rocket: Test integration

--- src/SlackCallbackController.php
+++ src/SlackCallbackController.php
@@ -7,7 +7,7 @@ class SlackCallbackController
     public function handler(?array $data): array
     {
         $arquivo = fopen(__DIR__ . '/meuarquivo.txt', 'w');
-        fwrite($arquivo, implode(PHP_EOL, $data));
+        fwrite($arquivo, implode(PHP_EOL, $data ?? []));
         fclose($arquivo);
 
         return ['ok'];
